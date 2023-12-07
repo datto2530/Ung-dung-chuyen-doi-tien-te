dac4fabe0fef00213e4e6078a666bf9e0a604a38
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-07 17:03:21 +0700

    Da xong bai tap chuyen doi tien te

---
+++ Ung dung chuyen doi tien te/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+namespace UsingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Usd;
+            float Vnd = 23000f;
+            Console.WriteLine("Nhap so tien muon quy doi: ");
+            Usd = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ti gia hien tai la 1 Usd doi duoc " +Vnd);
+            Console.WriteLine("So tien chuyen doi theo ty gia hien tai la:" + Usd * Vnd);
+            Console.ReadKey();
+        }
+    }
+}
